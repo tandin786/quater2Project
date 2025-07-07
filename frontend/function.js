9195d81a2d2fa04eadbf30734e2571de12a111f2
shadow <darkshadowblaze786@g.mail.com>
2025-07-07 10:29:32 +0600

    seaech function

--- frontend/function.js
+++ frontend/function.js
@@ -1,3 +1,32 @@
-function greeting(hello) {
-  return greeting;
+const mealsnameClass =
+  "text-black rounded-b-lg text-2xl font-bold uppercase text-center bg-white/100 w-70";
+const mealsimageClass = "bg-white rounded-t-lg p-4 h-70 w-auto";
+const display = document.getElementById("display");
+const container = document.getElementById("container");
+
+function addFoods(foodsname, foodimageURL) {
+  const result = document.createElement("div");
+  container.appendChild(result);
+
+  const image = document.createElement("img");
+  image.setAttribute("src", foodimageURL);
+  image.setAttribute("class", mealsimageClass);
+  result.appendChild(image);
+
+  const name = document.createElement("p");
+  name.innerText = foodsname;
+  name.setAttribute("class", mealsnameClass);
+  result.appendChild(name);
+}
+function search() {
+  const disvalue = display.value;
+  axios
+    .get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${disvalue}`)
+    .then(function (response) {
+      console.log(response);
+      container.innerHTML = "";
+      response.data.meals.forEach((meal) => {
+        addFoods(meal.strMeal, meal.strMealThumb);
+      });
+    });
 }
