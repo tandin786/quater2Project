3717ad8a98c502571f350fe39e786dba8522a287
Chakar Pema Nyingpo <chakarpemanyingpo@Chakars-MacBook-Air.local>
2025-07-09 09:23:35 +0600

    Updates on function and front-end

---
+++ frontend/ingredients.js
@@ -0,0 +1,53 @@
+ingredients = [
+  {
+    Items: "Chicken",
+    Image: "https://www.themealdb.com/images/ingredients/chicken-medium.png",
+  },
+  {
+    Items: "Salmon",
+    Image: "https://www.themealdb.com/images/ingredients/salmon-medium.png",
+  },
+  {
+    Items: "Beef",
+    Image: "https://www.themealdb.com/images/ingredients/beef-medium.png",
+  },
+  {
+    Items: "Beef Fillet",
+    Image:
+      "https://www.themealdb.com/images/ingredients/beef%20fillet-medium.png",
+  },
+  {
+    Items: "Pork",
+    Image: "https://www.themealdb.com/images/ingredients/pork-medium.png",
+  },
+  {
+    Items: "Rolled Oats",
+    Image:
+      "https://www.themealdb.com/images/ingredients/rolled%20oats-medium.png",
+  },
+  {
+    Items: "Hard Taco Shells",
+    Image:
+      "https://www.themealdb.com/images/ingredients/hard%20taco%20shells-medium.png",
+  },
+  {
+    Items: "Jalapeno",
+    Image: "",
+  },
+  {
+    Items: "Mushrooms",
+    Image: "https://www.themealdb.com/images/ingredients/jalapeno-medium.png",
+  },
+  {
+    Items: "Rice Stick Noodles",
+    Image:
+      "https://www.themealdb.com/images/ingredients/Rice%20Stick%20Noodles-medium.png",
+  },
+  {
+    Items: "Smoked Paprika",
+    Image:
+      "https://www.themealdb.com/images/ingredients/Smoked%20Paprika-medium.png",
+  },
+];
+
+module.exports = ingredients;
